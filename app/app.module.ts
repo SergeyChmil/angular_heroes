8035e80f46289b4351b2df3b638faeea04be0197
SergeyTchmil <tchmil@syneforge.com>
2016-12-19 18:19:38 +0200

    added new changes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent }  from './app.component';
-import {HeroDetailComponent} from "./hero-detail.component";
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
